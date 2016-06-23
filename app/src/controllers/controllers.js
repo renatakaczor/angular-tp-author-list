554a1250317c3e3779fd06063c029dae0915af22
Thibault <thibault.lebourgeois@gmail.com>
2016-06-23 12:49:44 +0200

    main page

--- app/src/controllers/controllers.js
+++ app/src/controllers/controllers.js
@@ -8,12 +8,10 @@ app.controller('mainCtrl', function($scope, $http){
 })
 
 app.controller('authorCtrl', function($scope, $routeParams, $http) {
-	console.log("author controller", $routeParams.author_id);
 	$http.get('app/resources/serveur.php', 
 	{
-		params: {method: 'getAuteurById', author_id:$routeParams.author_id}
+		params: {method: 'getAuteurById', id_auteur:$routeParams.author_id}
 	}).then(function(result) {
-		console.log(result.data);
-		$scope.auteur = result.data[0];
+		$scope.author = result.data[$routeParams.author_id];
 	})
 })
